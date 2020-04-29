29c381dd6eff67cab114ee26f1d746abccb2f29b
Nobuhiro Kuroiwa <tacobell@gj9.so-net.ne.jp>
2020-04-29 12:44:05 +0900

    add code using local memory code add code using local memory

--- src/AutoCorrelatioinBench/acorr.cl
+++ src/AutoCorrelatioinBench/acorr.cl
@@ -18,3 +18,36 @@ __kernel void acorr(
     // copy local buf to ouput buffer
     output[global_id] = sum;
 }
+
+__kernel void acorr_local(
+    const    int     N,
+    __global float* sample,
+    __global float* output,
+    __local  float* locbuf)
+{
+    const int global_id  = get_global_id(0);
+    const int group_id    = get_group_id(0);
+    const int local_size  = get_local_size(0);
+    const int local_id    = get_local_id(0);
+
+    event_t evt_copy_in = async_work_group_copy(locbuf,
+                                                sample + group_id * local_size,
+                                                local_size,
+                                                0);
+
+    const int tau = global_id;
+    float sum     = 0.0f;
+    for(int i = 0; i < N - tau; i++) {
+        //printf("[%d]+=[%d]*[%d]\n", global_id, i, i+tau);
+        sum += (locbuf[i] * locbuf[i + tau]);
+    }
+    // copy local buf to ouput buffer
+    locbuf[global_id] = sum;
+    barrier(CLK_LOCAL_MEM_FENCE);
+    event_t evt_copy_out = async_work_group_copy(sample + local_size * group_id,
+                                                 locbuf,
+                                                 local_size,
+                                                 0);
+    wait_group_events(1, &evt_copy_out);
+}
+
--- src/AutoCorrelatioinBench/acorr.cpp
+++ src/AutoCorrelatioinBench/acorr.cpp
@@ -22,10 +22,13 @@
 #if defined(__APPLE__)
 // my personal debug setting. need to change later
 #define CL_PATH         "/Users/nobu/GitHub/AutoCorrelationBench/src/AutoCorrelatioinBench/acorr.cl"
+#define DEVICE_ID       1
 #else
 #define CL_PATH         "acorr.cl"
+#define DEVICE_ID       0
 #endif
-#define SAMPLE_SIZE_N   1024       // Sample Size
+#define SAMPLE_SIZE_N   1024      // Sample Size
+#define WORK_GROUP_SIZE 128       // Workgroup size
 
 //------------------------------------------------------------------------------
 //  functions
@@ -44,12 +47,12 @@ int main(int argc, char *argv[])
 	for ( int i = 0; i < h_sample.size(); i++) {
 		h_sample[i]   = (float)sin(3.5 * i * M_PI / N);
         h_sample[i+1] = 0.0f;
-        cout << "orig[" << i << "] = " << h_sample[i] << endl;
+        //cout << "orig[" << i << "] = " << h_sample[i] << endl;
 	}
 
     try
     {
-        cl_uint deviceIndex = 0;
+        cl_uint deviceIndex = DEVICE_ID;
 
         // Get list of devices
         std::vector<cl::Device> devices;
@@ -76,7 +79,6 @@ int main(int argc, char *argv[])
 
         // Create the compute program from the source buffer
         cl::Program program = cl::Program(context, util::loadProgram(CL_PATH), true);
-        cl::make_kernel<int, cl::Buffer, cl::Buffer> acorr( program, "acorr");
 
         // Create the compute kernel from the program
         cl::NDRange global(N);
@@ -87,21 +89,49 @@ int main(int argc, char *argv[])
                                          sizeof(h_sample[0]) * h_sample.size()
                                          );
 
-        double start_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0;
-        for(int i = 0; i < 10000; i++) {
-            // compute
-            acorr(cl::EnqueueArgs( queue, global),
-                  N,
-                  d_sample,
-                  d_output
-                  );
-            queue.finish();
-        }
-        double run_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0 - start_time;
+        // bench mark for basic kernel
+        {
+            cl::make_kernel<int, cl::Buffer, cl::Buffer> acorr( program, "acorr");
+            double start_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0;
+            for(int i = 0; i < 10000; i++) {
+                // compute
+                acorr(cl::EnqueueArgs( queue, global),
+                      N,
+                      d_sample,
+                      d_output
+                      );
+                queue.finish();
+            }
+            double run_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0 - start_time;
 
-        std::vector<float> h_output( N, 0);
-        cl::copy(queue, d_output, h_output.begin(), h_output.end());
-        cout << run_time << " sec" << endl;
+            std::vector<float> h_output( N, 0);
+            cl::copy(queue, d_output, h_output.begin(), h_output.end());
+            cout << run_time << " sec" << endl;
+        }
+
+        // bench mark using local mem
+        {
+            cl::NDRange local(WORK_GROUP_SIZE);
+            cl::make_kernel<int, cl::Buffer, cl::Buffer, cl::LocalSpaceArg> acorr_local( program, "acorr_local");
+            cl::LocalSpaceArg localmem = cl::Local(sizeof(float) * WORK_GROUP_SIZE);
+
+            double start_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0;
+            for(int i = 0; i < 10000; i++) {
+                // compute
+                acorr_local(cl::EnqueueArgs( queue, global, local),
+                      N,
+                      d_sample,
+                      d_output,
+                      localmem
+                      );
+                queue.finish();
+            }
+            double run_time   = static_cast<double>( timer.getTimeMilliseconds()) / 1000.0 - start_time;
+
+            std::vector<float> h_output( N, 0);
+            cl::copy(queue, d_output, h_output.begin(), h_output.end());
+            cout << run_time << " sec" << endl;
+        }
     } catch (cl::Error err) {
         std::cout << "Exception\n";
         std::cerr << "ERROR: "
